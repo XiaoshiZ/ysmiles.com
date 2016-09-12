9dfdc401386b14d60fe6b655e8ce232ae816101f
ysfree <tshu92@gmail.com>
2016-09-12 19:06:17 -0400

    update createPost.py and learning Vim

--- creatPost.py
+++ creatPost.py
@@ -1,22 +1,32 @@
 from time import strftime
 
-layout
-title
-subtitle
-author
-header-img
-catalog
-herader-mask
-tags
----
-layout:     post
-title:      "First Post - how to make a website by Jekyll"
-subtitle:   " \"Hello World, Hello Blog\""
-author:     "YS"
-date:       2016-05-17 12:27:00 -0500
-header-img: "img/post-bg-2015.jpg"
-catalog:    true
-header-mask: 0.3
-tags:
-    - Life
----
+title = input("title: ")
+subtitle = input("subtitle: ")
+author = input("author: ") or 'YS'
+date = strftime("%Y-%m-%d %H:%M") 
+headerimg = input("header-img: ")
+catalog = input("catalog: ") or 'true'
+headermask = input("herader-mask: ")
+tags = input("tags: ") or 'unclassified'
+tags = tags.split()
+
+filename =  '_posts/' + date[:10] + '-' + '-'.join(title.split()) + '.md'
+if headerimg != '':
+    imgname = '"img/' + headerimg + '"'
+else:
+    imgname = ''
+
+with open(filename, 'w') as myfile:
+    myfile.write('---\n')
+    myfile.write('layout:     post\n')
+    myfile.write('title:      "' + title + '"\n')
+    myfile.write('subtitle:   "' + subtitle + '"\n')
+    myfile.write('author:     "' + author + '"\n')
+    myfile.write('date:       ' + date + '\n')
+    myfile.write('header-img: ' + imgname + '\n')
+    myfile.write('catalog:    ' + catalog + '\n')
+    myfile.write('header-mask: ' + headermask + '\n')
+    myfile.write('tags:\n')
+    for tag in tags:
+        myfile.write('    - ' + tag + '\n')
+    myfile.write('---\n')
