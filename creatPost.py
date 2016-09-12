0e35416f6b04af87cd2bfa98e6a8ad0316c342d6
ysfree <tshu92@gmail.com>
2016-09-12 17:25:51 -0400

    test tags

---
+++ creatPost.py
@@ -0,0 +1,22 @@
+from time import strftime
+
+layout
+title
+subtitle
+author
+header-img
+catalog
+herader-mask
+tags
+---
+layout:     post
+title:      "First Post - how to make a website by Jekyll"
+subtitle:   " \"Hello World, Hello Blog\""
+author:     "YS"
+date:       2016-05-17 12:27:00 -0500
+header-img: "img/post-bg-2015.jpg"
+catalog:    true
+header-mask: 0.3
+tags:
+    - Life
+---
